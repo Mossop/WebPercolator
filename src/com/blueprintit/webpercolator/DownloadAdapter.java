e251b76499c2df5db3400cae5c2f27e641a97a53
Dave Townsend <dtownsend@oxymoronical.com>
2004-04-04 21:57:01 +0000

    Added an adapter for the DownloadListener

---
+++ src/com/blueprintit/webpercolator/DownloadAdapter.java
@@ -0,0 +1,42 @@
+package com.blueprintit.webpercolator;
+
+/**
+ * @author Dave
+ */
+public class DownloadAdapter implements DownloadListener
+{
+	/* (non-Javadoc)
+	 * @see com.blueprintit.webpercolator.DownloadListener#downloadStarted(com.blueprintit.webpercolator.DownloadEvent)
+	 */
+	public void downloadStarted(DownloadEvent e)
+	{
+	}
+
+	/* (non-Javadoc)
+	 * @see com.blueprintit.webpercolator.DownloadListener#downloadUpdate(com.blueprintit.webpercolator.DownloadEvent)
+	 */
+	public void downloadUpdate(DownloadEvent e)
+	{
+	}
+
+	/* (non-Javadoc)
+	 * @see com.blueprintit.webpercolator.DownloadListener#downloadComplete(com.blueprintit.webpercolator.DownloadEvent)
+	 */
+	public void downloadComplete(DownloadEvent e)
+	{
+	}
+
+	/* (non-Javadoc)
+	 * @see com.blueprintit.webpercolator.DownloadListener#downloadFailed(com.blueprintit.webpercolator.DownloadEvent)
+	 */
+	public void downloadFailed(DownloadEvent e)
+	{
+	}
+
+	/* (non-Javadoc)
+	 * @see com.blueprintit.webpercolator.DownloadListener#downloadRedirected(com.blueprintit.webpercolator.DownloadEvent)
+	 */
+	public void downloadRedirected(DownloadEvent e)
+	{
+	}
+}
